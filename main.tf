18b16c41ff635edf9c13ae5b7db917dcf2e3ff15
mulmuri <gyumin.oh@payhere.in>
2024-10-10 04:49:53 +0900

    feat: gke module entry

--- main.tf
+++ main.tf
@@ -3,3 +3,10 @@ terraform {
         path = "/srv/atlantis/terraform.tfstate"
     }
 }
+
+module "gke" {
+    source = "../../modules/gcp/gke"
+    project_id = var.project_id
+    region = var.region
+    zone = var.zone
+}
