50b74e7663de996d7dc521c125d9f3fc47aa98b9
mulmuri <gyumin.oh@payhere.in>
2025-01-25 06:36:08 +0900

    feat: GCP IAM

--- projects/dev/main.tf
+++ projects/dev/main.tf
@@ -7,26 +7,10 @@ module "service" {
     source = "../../modules/gcp/service"
     project_id = var.project_id
 }
-/*
+
 module "iam" {
     source = "../../modules/gcp/iam"
     project_id = var.project_id
     region = var.region
     depends_on = [module.service]
 }
-*/
-
-
-/*
-module "gke" {
-    source = "../../modules/gcp/gke"
-    project_id = var.project_id
-    region = var.region
-    zone = var.zone
-}
-
-module "harbor" {
-    source = "../../modules/infra/harbor"
-    depends_on = [module.gke]
-}
-*/
