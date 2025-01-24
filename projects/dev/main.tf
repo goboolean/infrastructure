61a96e134cc0119b6082ef96f13c7d980c18c4fc
mulmuri <gyumin.oh@payhere.in>
2025-01-24 22:40:53 +0900

    resolve conflict

--- projects/dev/main.tf
+++ projects/dev/main.tf
@@ -1,12 +1,27 @@
 terraform {
     backend "local" {
-        path = "/srv/atlantis/terraform.tfstate"
     }
 }
 
+module "iam" {
+    source = "../../modules/gcp/iam"
+    project_id = var.project_id
+    region = var.region
+}
+
+
+
+
+/*
 module "gke" {
     source = "../../modules/gcp/gke"
     project_id = var.project_id
     region = var.region
     zone = var.zone
 }
+
+module "harbor" {
+    source = "../../modules/infra/harbor"
+    depends_on = [module.gke]
+}
+*/
