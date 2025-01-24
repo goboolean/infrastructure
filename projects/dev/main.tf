0864ececc7ecb896b44067bb012710a28754dafe
mulmuri <gyumin.oh@payhere.in>
2025-01-25 07:38:41 +0900

    fix: format

--- projects/dev/main.tf
+++ projects/dev/main.tf
@@ -14,10 +14,3 @@ module "iam" {
     region = var.region
     depends_on = [module.service]
 }
-
-module "gce" {
-    source = "../../modules/gcp/gce"
-    zone = var.zone
-    service_account_email = module.iam.atlantis_service_account_email
-    depends_on = [module.service]
-}
