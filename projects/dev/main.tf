f6edbe67ce8a4da0eb377f5407d5ca0b017f03ae
mulmuri <gyumin.oh@payhere.in>
2025-01-25 00:27:43 +0900

    feat: enable gcp apis

--- projects/dev/main.tf
+++ projects/dev/main.tf
@@ -3,13 +3,18 @@ terraform {
     }
 }
 
+module "service" {
+    source = "../../modules/gcp/service"
+    project_id = var.project_id
+}
+/*
 module "iam" {
     source = "../../modules/gcp/iam"
     project_id = var.project_id
     region = var.region
+    depends_on = [module.service]
 }
-
-
+*/
 
 
 /*
