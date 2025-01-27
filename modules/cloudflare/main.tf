4ab5ee99cbc6582a1a99f97bb0b64d0ebc9ba201
mulmuri <gyumin.oh@payhere.in>
2025-01-28 02:27:18 +0900

    feat: cloudflare certificate

--- modules/cloudflare/main.tf
+++ modules/cloudflare/main.tf
@@ -6,3 +6,13 @@ resource "cloudflare_dns_record" "atlantis" {
   ttl = 1
   type = "A"
 }
+
+resource "cloudflare_certificate_pack" "example_certificate_pack" {
+  zone_id = var.zone_id
+  certificate_authority = "letsencrypt"
+  hosts = ["*.goboolean.io"]
+  type = "advanced"
+  validation_method = "txt"
+  validity_days = 14
+  cloudflare_branding = false
+}
