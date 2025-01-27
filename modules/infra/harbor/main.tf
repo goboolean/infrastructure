f10bac01f396d7ef3e16b78b26aae73d146af6e9
mulmuri <gyumin.oh@payhere.in>
2025-01-27 19:52:38 +0900

    feat: update harbor namespace

--- modules/infra/harbor/main.tf
+++ modules/infra/harbor/main.tf
@@ -2,7 +2,9 @@ resource "helm_release" "harbor" {
   name       = "harbor"
   repository = "https://helm.goharbor.io"
   chart      = "harbor"
-  version    = "1.15.0"
+  namespace        = "harbor"
+  create_namespace = true
+  version          = "1.15.0"
 
   values = [file("${path.module}/values.yaml")]
 }
