71f6e22d13c88f92b2457ce4dd926b7153bceb2a
mulmuri <gyumin.oh@payhere.in>
2025-01-28 17:38:14 +0900

    feat: remove unnesecessery lables

--- modules/gcp/gke/namespace/main.tf
+++ modules/gcp/gke/namespace/main.tf
@@ -1,18 +1,12 @@
 resource "kubernetes_namespace" "argocd" {
   metadata {
     name = "argocd"
-    labels = {
-      name = "argocd"
-    }
   }
 }
 
 resource "kubernetes_namespace" "cert_manager" {
   metadata {
     name = "cert-manager"
-    labels = {
-      name = "cert-manager"
-    }
   }
 }
 
