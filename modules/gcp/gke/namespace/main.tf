b6465bff78e58683fc70ed3345be915abc28dee0
mulmuri <gyumin.oh@payhere.in>
2025-01-28 17:36:31 +0900

    feat: add lables on namespace

--- modules/gcp/gke/namespace/main.tf
+++ modules/gcp/gke/namespace/main.tf
@@ -1,17 +1,26 @@
 resource "kubernetes_namespace" "argocd" {
   metadata {
     name = "argocd"
+    labels = {
+      name = "argocd"
+    }
   }
 }
 
 resource "kubernetes_namespace" "cert_manager" {
   metadata {
     name = "cert-manager"
+    labels = {
+      name = "cert-manager"
+    }
   }
 }
 
 resource "kubernetes_namespace" "harbor" {
   metadata {
     name = "harbor"
+    labels = {
+      name = "harbor"
+    }
   }
 }
