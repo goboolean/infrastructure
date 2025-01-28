6156589b5d14fe814090c2733f66ea7ce928031e
mulmuri <gyumin.oh@payhere.in>
2025-01-28 18:26:58 +0900

    fix: deploy helm first

--- modules/infra/cert-manager/manifest.tf
+++ modules/infra/cert-manager/manifest.tf
@@ -1,3 +1,4 @@
+/*
 resource "kubernetes_secret" "cloudflare_api_token" {
   metadata {
     name      = "cloudflare-api-token-secret"
@@ -9,20 +10,15 @@ resource "kubernetes_secret" "cloudflare_api_token" {
   }
 }
 
-resource "kubectl_manifest" "cluster_issuer" {
-  yaml_body = file("${path.module}/cluster-issuer.yaml")
-  metadata {
-    namespace = "cert-manager"
-  }
+resource "kubernetes_manifest" "cluster_issuer" {
+  manifest = yamldecode(file("${path.module}/cluster-issuer.yaml"))
 
   depends_on = [kubernetes_secret.cloudflare_api_token]
 }
 
-resource "kubectl_manifest" "wildcard_certificate" {
-  yaml_body = file("${path.module}/certificate.yaml")
-  metadata {
-    namespace = "cert-manager"
-  }
+resource "kubernetes_manifest" "wildcard_certificate" {
+  manifest = yamldecode(file("${path.module}/certificate.yaml"))
 
-  depends_on = [kubectl_manifest.cluster_issuer]
+  depends_on = [kubernetes_manifest.cluster_issuer]
 }
+*/
