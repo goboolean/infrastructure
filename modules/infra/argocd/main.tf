85cd144c7783191e89a0946b511bb5d32837c867
mulmuri <gyumin.oh@payhere.in>
2025-01-30 23:09:59 +0900

    feat: argocd roles

--- modules/infra/argocd/main.tf
+++ modules/infra/argocd/main.tf
@@ -1,20 +1,32 @@
-module "kustomize_argocd" {
-  source  = "kbst.xyz/catalog/custom-manifests/kustomization"
-  version = "0.3.0"
-
-  configuration_base_key = "default"
-  configuration = {
-    default = {
-      namespace = "argocd"
-
-      resources = [
-        "${path.module}/kustomization.yaml",
-      ]
-    }
-  }
-}
-
+#module "kustomize_argocd" {
+#  source  = "kbst.xyz/catalog/custom-manifests/kustomization"
+#  version = "0.3.0"
+#
+#  configuration_base_key = "default"
+#  configuration = {
+#    default = {
+#      namespace = "argocd"
+#
+#      resources = [
+#        "${path.module}/kustomization.yaml",
+#      ]
+#    }
+#  }
+#}
+#
 resource "kubernetes_manifest" "argocd_gateway" {
   manifest = yamldecode(file("${path.module}/gateway.yaml"))
-  depends_on = [helm_release.argocd]
 }
+
+resource "kubernetes_manifest" "argocd_repo_server_role" {
+  manifest = yamldecode(file("${path.module}/argocd-repo-server-rbac.yaml"))
+}
+
+resource "kubernetes_manifest" "argocd_vault_plugin_role" {
+  manifest = yamldecode(file("${path.module}/argocd-vault-plugin-role.yaml"))
+}
+
+resource "kubernetes_manifest" "argocd_repo_server_rolebinding" {
+  manifest = yamldecode(file("${path.module}/argocd-vault-plugin-role-binding.yaml"))
+}
+
