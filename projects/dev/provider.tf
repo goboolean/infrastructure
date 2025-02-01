f6102c9df3614b8069a49127158a7c80ffcdc43d
mulmuri <gyumin.oh@payhere.in>
2025-02-02 03:35:02 +0900

    fix: arg name

--- projects/dev/provider.tf
+++ projects/dev/provider.tf
@@ -64,6 +64,6 @@ provider "kubectl" {
 
 provider "argocd" {
   server_addr = "argocd.goboolean.io:443"
-  argocd_username = module.argocd.admin_username
-  argocd_password = module.argocd.admin_password
+  username = module.argocd.admin_username
+  password = module.argocd.admin_password
 }
