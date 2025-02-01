9a08086a7dd115942592120529e2852dcddfd8c1
mulmuri <gyumin.oh@payhere.in>
2025-02-01 17:05:25 +0900

    feat: all things to right way

--- modules/infra/argocd/application/application.tf
+++ modules/infra/argocd/application/application.tf
@@ -1,6 +1,6 @@
-resource "argocd_application" "fetch-system-worker" {
+resource "argocd_application" "fetch-system-streams" {
   metadata {
-    name      = "fetch-system-worker"
+    name      = "fetch-system-streams"
     namespace = "argocd"
   }
 
@@ -27,15 +27,38 @@ resource "argocd_application" "fetch-system-worker" {
         self_heal   = true
         allow_empty = true
       }
+    }
+  }
+}
 
-      sync_options = []
-      retry {
-        limit = "5"
-        backoff {
-          duration     = "30s"
-          max_duration = "2m"
-          factor       = "2"
-        }
+resource "argocd_application" "fetch-system-worker" {
+  metadata {
+    name      = "fetch-system-worker"
+    namespace = "argocd"
+  }
+
+  cascade = true
+  wait    = true
+
+  spec {
+    project = "default"
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "fetch-system"
+    }
+
+    source {
+      repo_url        = "https://github.com/goboolean/manifests"
+      path            = "fetch-system.worker/kustomize/overlays/dev"
+      target_revision = "main"
+    }
+
+    sync_policy {
+      automated {
+        prune       = true
+        self_heal   = true
+        allow_empty = true
       }
     }
   }
