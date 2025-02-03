0de0d5c79004a57ae7aac743298188abb4fa2efb
ikjeong <jipark7937@gmail.com>
2025-02-03 22:04:47 +0900

    Chore: change indent

--- projects/dev/main.tf
+++ projects/dev/main.tf
@@ -1,102 +1,100 @@
 terraform {
-    backend "gcs" {
-        bucket = "goboolean-terraform-state"
-        prefix = "dev"
-    }
+  backend "gcs" {
+    bucket = "goboolean-terraform-state"
+    prefix = "dev"
+  }
 }
 
 module "service" {
-    source = "../../modules/gcp/service"
-    project_id = var.project_id
+  source = "../../modules/gcp/service"
+  project_id = var.project_id
 }
 
 module "iam" {
-    source = "../../modules/gcp/iam"
-    project_id = var.project_id
-    region = var.region
+  source = "../../modules/gcp/iam"
+  project_id = var.project_id
+  region = var.region
 }
 
 module "gce" {
-    source = "../../modules/gcp/gce"
-    zone = var.zone
-    service_account_email = module.iam.atlantis_service_account_email
+  source = "../../modules/gcp/gce"
+  zone = var.zone
+  service_account_email = module.iam.atlantis_service_account_email
 }
 
 module "cloudflare" {
-    source = "../../modules/cloudflare"
-    api_token = var.cloudflare_api_token
-    zone_id = var.cloudflare_zone_id
-    ip_address = module.istio.istio_gateway_ip
+  source = "../../modules/cloudflare"
+  api_token = var.cloudflare_api_token
+  zone_id = var.cloudflare_zone_id
+  ip_address = module.istio.istio_gateway_ip
 }
 
 module "gcs" {
-    source = "../../modules/gcp/gcs"
-    project_id = var.project_id
-    location = var.location
+  source = "../../modules/gcp/gcs"
+  project_id = var.project_id
+  location = var.location
 }
 
 module "gke" {
-    source = "../../modules/gcp/gke"
-    region = var.region
-    project_id = var.project_id
-    zone = var.zone
+  source = "../../modules/gcp/gke"
+  region = var.region
+  project_id = var.project_id
+  zone = var.zone
 }
 
 module "istio" {
-    source = "../../modules/infra/istio"
-    depends_on = [module.gke]
+  source = "../../modules/infra/istio"
+  depends_on = [module.gke]
 }
 
 module "harbor" {
-    source = "../../modules/infra/harbor"
-    depends_on = [module.gke, module.namespace]
+  source = "../../modules/infra/harbor"
+  depends_on = [module.gke, module.namespace]
 }
 
 module "acme" {
-    source = "../../modules/cloudflare/acme"
+  source = "../../modules/cloudflare/acme"
 
-    cloudflare_email = var.cloudflare_email
-    cloudflare_api_token = var.cloudflare_api_token
-    cloudflare_zone_id = var.cloudflare_zone_id
-    cloudflare_api_key = var.cloudflare_api_key
+  cloudflare_email = var.cloudflare_email
+  cloudflare_api_token = var.cloudflare_api_token
+  cloudflare_zone_id = var.cloudflare_zone_id
+  cloudflare_api_key = var.cloudflare_api_key
 }
 
 module "namespace" {
-    source = "../../modules/gcp/gke/namespace"
+  source = "../../modules/gcp/gke/namespace"
 }
 
 module "cert_manager" {
-    source = "../../modules/infra/cert-manager"
-    depends_on = [module.namespace]
-    cloudflare_api_token = var.cloudflare_api_token
+  source = "../../modules/infra/cert-manager"
+  depends_on = [module.namespace]
+  cloudflare_api_token = var.cloudflare_api_token
 }
 
 module "vault" {
-    source = "../../modules/infra/vault"
-    depends_on = [module.namespace]
+  source = "../../modules/infra/vault"
+  depends_on = [module.namespace]
 }
 
 module "argocd" {
-    source = "../../modules/infra/argocd"
-    depends_on = [module.namespace]
+  source = "../../modules/infra/argocd"
+  depends_on = [module.namespace]
 }
 
 module "kafka" {
-    source = "../../modules/infra/kafka"
-    depends_on = [module.namespace]
+  source = "../../modules/infra/kafka"
+  depends_on = [module.namespace]
 }
 
 module "etcd" {
-    source = "../../modules/infra/etcd"
-    depends_on = [module.namespace]
+  source = "../../modules/infra/etcd"
+  depends_on = [module.namespace]
 }
 
 module "opentelemetry" {
-    source = "../../modules/infra/opentelemetry"
-    depends_on = [module.namespace]
+  source = "../../modules/infra/opentelemetry"
+  depends_on = [module.namespace]
 }
-
-
 
 /*
   It is not possible to deploy all infrastructure with a single main.tf.
@@ -104,11 +102,10 @@ module "opentelemetry" {
   and the following variables can be injected starting from the second step,
   so they should be moved later.
 */
-
 module "argocd-application" {
-    source = "../../modules/infra/argocd/application"
-    depends_on = [module.argocd]
-    providers = {
-        argocd = argocd
-    }
+  source = "../../modules/infra/argocd/application"
+  depends_on = [module.argocd]
+  providers = {
+    argocd = argocd
+  }
 }
