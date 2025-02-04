8f68c0e8ac6997c2a9296819443bd55dc983503f
ikjeong <jipark7937@gmail.com>
2025-02-04 16:29:40 +0900

    fix: add influxdb gateway

--- modules/infra/fetch-system/influxdb/main.tf
+++ modules/infra/fetch-system/influxdb/main.tf
@@ -17,3 +17,8 @@ resource "helm_release" "influxdb" {
 
   timeout = 600
 }
+
+resource "kubernetes_manifest" "influxdb-gateway" {
+  manifest = yamldecode(file("${path.module}/gateway.yaml"))
+  depends_on = [helm_release.influxdb]
+}
