e9d7d5253e9665c402699c6bea1e964e36895750
ikjeong <jipark7937@gmail.com>
2025-02-04 17:54:03 +0900

    feat: update grafana resources and correct datasource name

--- modules/infra/fetch-system/grafana/main.tf
+++ modules/infra/fetch-system/grafana/main.tf
@@ -6,7 +6,7 @@ locals {
   })
 }
 
-resource "helm_release" "influxdb" {
+resource "helm_release" "grafana" {
   name             = "grafana"
   chart            = "grafana"
   namespace        = "grafana"
@@ -18,7 +18,7 @@ resource "helm_release" "influxdb" {
   timeout = 600
 }
 
-resource "kubernetes_manifest" "influxdb-gateway" {
+resource "kubernetes_manifest" "grafana-gateway" {
   manifest = yamldecode(file("${path.module}/gateway.yaml"))
   depends_on = [helm_release.influxdb]
 }
