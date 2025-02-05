c4bba9648eed90663a979197db5dc0f33220c454
ikjeong <jipark7937@gmail.com>
2025-02-06 03:46:05 +0900

    feat: change repo

--- modules/infra/kafka/main.tf
+++ modules/infra/kafka/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "kafka" {
   name             = "kafka"
   chart            = "kafka"
   namespace        = "kafka"
-  repository       = "https://charts.bitnami.com/bitnami"
+  repository       = "oci://registry-1.docker.io/bitnamicharts"
   version          = "31.3.1"
   
   values = [file("${path.module}/values.yaml")]
