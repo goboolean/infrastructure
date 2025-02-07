288562ce23608440bdd45473b58d43a362af2a51
ikjeong <jipark7937@gmail.com>
2025-02-07 15:04:39 +0900

    feat: add kafka exporter for consumer lag

--- modules/infra/kafka/main.tf
+++ modules/infra/kafka/main.tf
@@ -5,7 +5,21 @@ resource "helm_release" "kafka" {
   repository       = "oci://registry-1.docker.io/bitnamicharts"
   version          = "31.3.1"
   
-  values = [file("${path.module}/values.yaml")]
+  values = [file("${path.module}/kafka-values.yaml")]
 
   timeout = 600
+}
+
+resource "helm_release" "kafka-exporter" {
+  name             = "kafka-exporter"
+  chart            = "kafka-exporter"
+  namespace        = "kafka"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  version          = "2.11.0"
+  
+  values = [file("${path.module}/exporter-values.yaml")]
+
+  timeout = 600
+
+  depends_on = [helm_release.kafka]
 }
