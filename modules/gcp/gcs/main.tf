d72e614f67d6def4ae63803905de9eb2a67d1a5d
mulmuri <gyumin.oh@payhere.in>
2025-02-16 01:08:51 +0900

    feat: create stock-data bucket

--- modules/gcp/gcs/main.tf
+++ modules/gcp/gcs/main.tf
@@ -21,3 +21,17 @@ resource "google_storage_bucket" "loki" {
 
   public_access_prevention = "enforced"
 }
+
+resource "google_storage_bucket" "stock_data" {
+  name          = "${var.project_id}-stock-data"
+  location      = var.location
+  project       = var.project_id
+
+  versioning {
+    enabled = false
+  }
+
+  uniform_bucket_level_access = true
+
+  public_access_prevention = "enforced"
+}
