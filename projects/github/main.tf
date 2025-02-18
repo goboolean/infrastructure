88ca86b8ed96b2658471e809463fd6bd55cfc609
mulmuri <gyumin.oh@payhere.in>
2025-02-18 20:06:36 +0900

    feat: manage ga secret using hcl

--- projects/github/main.tf
+++ projects/github/main.tf
@@ -10,20 +10,37 @@ data "vault_kv_secret_v2" "github" {
   name = "github"
 }
 
+data "vault_kv_secret_v2" "harbor" {
+  mount = "kv"
+  name = "infra/harbor"
+}
+
 locals {
   github_token = data.vault_kv_secret_v2.github.data["admin_token"]
   atlantis_webhook_secret = data.vault_kv_secret_v2.github.data["atlantis_webhook_secret"]
+
+  harbor_username = data.vault_kv_secret_v2.harbor.data["username"]
+  harbor_password = data.vault_kv_secret_v2.harbor.data["password"]
 }
 
-module "github" {
-  source = "../../modules/github"
+module "repository" {
+  source = "../../modules/github/repository"
+}
+
+module "webhook" {
+  source = "../../modules/github/webhook"
   atlantis_webhook_secret = local.atlantis_webhook_secret
+}
+
+module "github_secret" {
+  source = "../../modules/github/github_secret"
+  registry_username = local.harbor_username
+  registry_password = local.harbor_password
 }
 
 provider "github" {
   owner = "goboolean"
   token = local.github_token
 }
-
 
 
