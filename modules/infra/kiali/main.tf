7be46ec6f18292c161b2d555628de3d2eb33059c
흙파는쥐 <68294539+mulmuri@users.noreply.github.com>
2025-02-19 15:28:39 +0900

    feat: refactor & deploy applications (#180)
    Co-authored-by: mulmuri <gyumin.oh@payhere.in>

--- modules/infra/kiali/main.tf
+++ modules/infra/kiali/main.tf
@@ -7,7 +7,8 @@ resource "helm_release" "kiali_operator" {
 
   values = [templatefile("${path.module}/values.yaml", {
     prometheus_url = "http://kube-prometheus-stack-prometheus.monitoring.svc.cluster.local:9090"
-    grafana_url = "http://kube-prometheus-stack-grafana.monitoring.svc.cluster.local"
+    grafana_external_url = "https://grafana.goboolean.io"
+    grafana_internal_url = "http://kube-prometheus-stack-grafana.monitoring.svc.cluster.local:80"
     grafana_username = var.grafana_username
     grafana_password = var.grafana_password
   })]
