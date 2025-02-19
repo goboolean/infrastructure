13ffb97719e3dd9c5327d133fc4c411c9ce271ed
mulmuri <gyumin.oh@payhere.in>
2025-02-19 11:52:45 +0900

    fix: grafana url

--- modules/infra/kiali/main.tf
+++ modules/infra/kiali/main.tf
@@ -7,7 +7,7 @@ resource "helm_release" "kiali_operator" {
 
   values = [templatefile("${path.module}/values.yaml", {
     prometheus_url = "http://kube-prometheus-stack-prometheus.monitoring.svc.cluster.local:9090"
-    grafana_url = "http://kube-prometheus-stack-grafana.monitoring.svc.cluster.local:3000"
+    grafana_url = "http://kube-prometheus-stack-grafana.monitoring.svc.cluster.local"
     grafana_username = var.grafana_username
     grafana_password = var.grafana_password
   })]
