d0d1cc3d8c629e5586a6091bb30ce5aa426cbb91
흙파는쥐 <68294539+mulmuri@users.noreply.github.com>
2025-02-19 13:25:10 +0900

    feat: fix kiali to display proper graph (#178)
    * feat: fix kiali to display proper graph
    
    * fix: grafana url
    
    ---------
    
    Co-authored-by: mulmuri <gyumin.oh@payhere.in>

--- modules/infra/kiali/main.tf
+++ modules/infra/kiali/main.tf
@@ -3,23 +3,16 @@ resource "helm_release" "kiali_operator" {
   repository       = "https://kiali.org/helm-charts"
   chart            = "kiali-operator"
   namespace        = "kiali"
+  version          = "2.5.0"
+
+  values = [templatefile("${path.module}/values.yaml", {
+    prometheus_url = "http://kube-prometheus-stack-prometheus.monitoring.svc.cluster.local:9090"
+    grafana_url = "http://kube-prometheus-stack-grafana.monitoring.svc.cluster.local"
+    grafana_username = var.grafana_username
+    grafana_password = var.grafana_password
+  })]
 }
 
 resource "kubernetes_manifest" "kiali_gateway" {
   manifest = yamldecode(file("${path.module}/gateway.yaml"))
-}
-
-locals {
-  kiali_values = templatefile("${path.module}/kiali.yaml", {
-    prometheus_url = "http://prometheus.monitoring.svc.cluster.local:9090"
-    prometheus_username = ""
-    prometheus_password = ""
-    grafana_url = "http://grafana.monitoring.svc.cluster.local:3000"
-    grafana_username = var.grafana_username
-    grafana_password = var.grafana_password
-  })
-}
-
-resource "kubernetes_manifest" "kiali" {  
-  manifest = yamldecode(file("${path.module}/kiali.yaml"))
 }
